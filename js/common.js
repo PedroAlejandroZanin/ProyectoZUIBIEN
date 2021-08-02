a84b7bf69463c7a92c966f6a9c193b0ae7ea752d
Juan Pablo Marchi <juanpablomarchi@gmail.com>
2021-08-02 11:00:28 -0300

    se linkeo formulario

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
-document.getElementById("top-nav-contact").addEventListener('click', ()=>{
+const contactForm = () => {
     Swal.fire({
         title: 'Contacto ZUIBIEN',
         text: 'Dejanos tus datos para poder contactarte',
         html: `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSccdW7iInfGUD9so9ncholje25AXf-ErGsHBdLWk0iBXgyEuQ/viewform?embedded=true" width="400" height="800" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`
     })
-})
+}
